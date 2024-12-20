140e3ed99328c3647cb3f4123c67c3bfef799a62
Bunny <73196784+bunny-foofoo@users.noreply.github.com>
2024-12-20 14:46:15 -0800

    reponsive

--- yoshie-dvd/src/pages/index.tsx
+++ yoshie-dvd/src/pages/index.tsx
@@ -15,14 +15,14 @@ export default function Home() {
           <h1 className="text-2xl font-bold tracking-tighter text-[hsl(340,50%,91%)] sm:text-[5rem]">
             yoshie go boing boing
           </h1>
-          <div className="bg-[hsl(0,0%,3%)] w-[48rem] h-[32rem] border-[hsl(0,0%,9%)] border-2">
+          <div className="bg-[hsl(0,0%,3%)] w-72 h-48 md:w-[48rem] md:h-[32rem] outline-[hsl(0,0%,32%)] outline outline-1">
             <motion.div
-              className="bg-[hsl(0,0%,3%)] w-32 h-32 border-[hsl(0,0%,9%)] border-2"
+              className="bg-[hsl(0,0%,3%)] md:h-32 md:w-32 h-12 w-12 outline outline-[hsla(1,86%,49%,0.25)] outline-1"
               layoutId="yoshie"
               animate={{
-                x: [0, 640, 0],
-                y: [0, 384, 0],
-               }}
+                x: ["0%", "500%", "0%"],
+                y: ["0%", "300%", "0%"],
+              }}
               transition={{
                 x: {duration: 5, repeat: Infinity, ease: "linear"},
                 y: {duration: 7, repeat: Infinity, ease: "linear"}
@@ -33,6 +33,7 @@ export default function Home() {
                 width={128}
                 height={128}
                 alt="yoshie"
+                className="md:h-32 md:w-32 h-12 w-12"
               />
             </motion.div>
           </div>
