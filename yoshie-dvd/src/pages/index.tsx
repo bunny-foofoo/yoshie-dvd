02f6ccd5d8b33d96e632916fffe57c4c5d09bfd0
Bunny <73196784+bunny-foofoo@users.noreply.github.com>
2024-12-20 17:05:03 -0800

    bouncy count and spinny spin

--- yoshie-dvd/src/pages/index.tsx
+++ yoshie-dvd/src/pages/index.tsx
@@ -1,8 +1,38 @@
 import Head from "next/head";
 import Image from "next/image";
 import { motion } from "motion/react";
+import { useState } from "react";
+
+const funText = (boings: number) => {
+  if (boings >= 100) {
+    return "yoshie, blink if you need help";
+  } else if (boings >= 50) {
+    return "yoshie is godlike!";
+  } else if (boings >= 35) {
+    return "yoshie is unstoppable!";
+  } else if (boings >= 25) {
+    return "yoshie, dominating!";
+  } else if (boings >= 15) {
+    return "i wonder if she is tired?";
+  } else if (boings >= 10) {
+    return "wow! she's on a roll!";
+  } else if (boings >= 5) {
+    return "she's having fun!";
+  } else if (boings >= 3) {
+    return "good for her!";
+  }
+  return "";
+}
 
 export default function Home() {
+
+  const [bounces, setBounces] = useState(0);
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const addBounce = () => {
+    setBounces(bounces + 1);
+  };
+
   return (
     <>
       <Head>
@@ -15,6 +45,12 @@ export default function Home() {
           <h1 className="text-2xl font-bold tracking-tighter text-[hsl(340,50%,91%)] sm:text-[5rem]">
             yoshie go boing boing
           </h1>
+          <p className="text-[hsl(340,50%,91%)] text-lg">
+            {bounces === 0 && "yoshie has yet to hit a corner..."}
+            {bounces > 0 ? "yoshie has hit a corner " : ""}
+            {bounces === 1 && "once." || bounces > 1 && `${bounces} times. `}
+            {funText(bounces)}
+          </p>
           <div className="bg-[hsl(0,0%,3%)] w-72 h-48 md:w-[48rem] md:h-[32rem] outline-[hsl(0,0%,32%)] outline outline-1">
             <motion.div
               className="bg-[hsl(0,0%,3%)] md:h-32 md:w-32 h-12 w-12 outline outline-[hsla(1,86%,49%,0.25)] outline-1"
@@ -22,10 +58,25 @@ export default function Home() {
               animate={{
                 x: ["0%", "500%", "0%"],
                 y: ["0%", "300%", "0%"],
+                rotate: isSpinning ? [360, 0] : 0
               }}
+              onUpdate={
+                (latest) => {
+                  if ((latest.x === "0%" || latest.x === "500%") && (latest.y === "0%" || latest.y === "300%")) {
+                    addBounce();
+
+                    // unsure about this approach
+                    setIsSpinning(true);
+                    setTimeout(() => {
+                      setIsSpinning(false);
+                    }, 750);
+                  }
+                }
+              }
               transition={{
                 x: {duration: 5, repeat: Infinity, ease: "linear"},
-                y: {duration: 7, repeat: Infinity, ease: "linear"}
+                y: {duration: 7, repeat: Infinity, ease: "linear"},
+                rotate: {duration: 0.75, ease: "easeIn"}
               }}
               >
               <Image
